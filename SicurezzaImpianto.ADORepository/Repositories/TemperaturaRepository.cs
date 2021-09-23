e9d90cf40163281a89a598dc1f17d77f031e869f
Annamaria Ciasca <annamaria.ciasca@avanade.com>
2021-09-23 11:43:16 +0200

    Aggiunta Repository ADO

---
+++ SicurezzaImpianto.ADORepository/Repositories/TemperaturaRepository.cs
@@ -0,0 +1,48 @@
+﻿using SicurezzaImpianto.Core.Entities;
+using SicurezzaImpianto.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SicurezzaImpianto.ADORepository
+{
+    public class TemperaturaRepository : ITemperaturaRepository
+    {
+        const string connectionString = @"Data Source = (localdb)\MSSQLLocalDB;" +
+                                       "Initial Catalog = AcademyI;" +
+                                       "Integrated Security = true";
+        public List<Temperatura> GetItemsWithOutState()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandType = System.Data.CommandType.Text;
+            command.CommandText = "SELECT * FROM dbo.Temperatura WHERE Stato IS NULL";
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            List<Temperatura> temperature = new List<Temperatura>();
+
+            while (reader.Read())
+            {
+                Temperatura temperatura = new Temperatura();
+                temperatura.Id = (int)reader["Id"];
+                temperatura.DataMisurazione = Convert.ToDateTime(reader["DataMisurazione"]);
+                temperatura.OraMisurazione = (TimeSpan)reader["OraMisurazione"];
+                temperatura.ValoreTemperatura = Convert.ToDouble(reader["ValoreTemperatura"]);
+
+                temperature.Add(temperatura);
+            }
+            return temperature;
+        }
+
+
+    }
+}
+}
